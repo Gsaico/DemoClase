6a51d019d1894ad4b04bb18e2fadabe577563b85
Percy <gsaico@icloud.com>
2016-11-05 18:11:36 -0500

    Revert "Estructura Inicial Del Proyecto"
    This reverts commit 34a79516c0d2cd0e2b0edffdd53477fde2e248d5.
    
    # Conflicts:
    #	WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
    
    #	WindowsFormsApplication1/WindowsFormsApplication1/WindowsFormsApplication1.csproj

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< HEAD
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.Boton5 = new System.Windows.Forms.Button();
@@ -35,17 +36,19 @@
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.button2 = new System.Windows.Forms.Button();
+=======
+            this.button1 = new System.Windows.Forms.Button();
+>>>>>>> parent of 00a1bd7... Button 3
             this.SuspendLayout();
             // 
-            // button3
+            // button1
             // 
-            this.button3.BackColor = System.Drawing.Color.Red;
-            this.button3.Location = new System.Drawing.Point(88, 159);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 76);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = false;
+            this.button1.Location = new System.Drawing.Point(209, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
@@ -100,12 +103,16 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+<<<<<<< HEAD
             this.Controls.Add(this.button2);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.Boton5);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
+=======
+            this.Controls.Add(this.button1);
+>>>>>>> parent of 00a1bd7... Button 3
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -115,6 +122,7 @@
 
         #endregion
 
+<<<<<<< HEAD
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button Boton5;
@@ -122,6 +130,9 @@
         private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Button button2;
+=======
+        private System.Windows.Forms.Button button1;
+>>>>>>> parent of 00a1bd7... Button 3
     }
 }
 
